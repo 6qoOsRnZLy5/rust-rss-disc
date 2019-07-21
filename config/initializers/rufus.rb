a2bc72497a8a72830a2faca37d0ace14ab7ccb92
Your Name <you@example.com>
2019-07-21 22:10:56 +0200

    lul

--- config/initializers/rufus.rb
+++ config/initializers/rufus.rb
@@ -1,18 +1,36 @@
 require 'rufus-scheduler'
-require 'rake'
-
+require 'uri'
+require 'feedjira'
 
 
 if ENV["OC_DEPLOYMENT"] == "YES"
-  load File.join('lib', 'tasks', 'feed_importer.rake')
 
   scheduler = Rufus::Scheduler.new
 
-  scheduler.interval '3m' do
+  scheduler.interval '10m' do
     puts "next rufus run! It's #{Time.now}"
-    task = Rake::Task["feed_importer:pull"] 
-    task.reenable 
-    task.invoke 
+
+    Feed.all.each do |feed|
+      if feed.pullurl
+        if feed.pullurl =~ URI::regexp
+          puts "start syncing #{feed.title}"
+          content = Feedjira::Feed.fetch_and_parse feed.pullurl
+          content.entries.each do |entry|
+            local_entry = feed.entries.where(guid: entry.guid).first_or_initialize
+            local_entry.update_attributes(
+              description: entry.description, 
+              title: entry.title)
+            puts "Synced Entry - #{entry.title}"
+          end
+          puts "done syncing #{feed.title}"
+        else
+          puts "skipping #{feed.title}, pullurl is not an url"
+        end
+      else
+        puts "skipping #{feed.title}, no pullurl"
+      end
+    end
+
     puts "rufus task should be done! It's #{Time.now}"
   end
 
