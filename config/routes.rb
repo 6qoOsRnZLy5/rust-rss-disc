b026936a9f18fbca3a94f74e2993544396d4afce
Your Name <you@example.com>
2019-07-21 22:38:28 +0200

    lul

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   root to: "feeds#index"
 
   resources :feeds do
-    collection do
-      get 'refresh'
-    end
     resources :entries
   end
+
+  get 'refresh' to: 'feeds#refresh'
 
 end
