fa2952eaa91cce2ef92fc1ea999af3f0cc81eb3c
Your Name <you@example.com>
2019-07-21 21:58:18 +0200

    lul

--- config/initializers/rufus.rb
+++ config/initializers/rufus.rb
@@ -1,14 +1,13 @@
 require 'rufus-scheduler'
 require 'rake'
+load File.join(RAILS_ROOT, 'lib', 'tasks', 'feed_importer.rake')
+
 
 if ENV["OC_DEPLOYMENT"] == "YES"
-
-  load File.join(RAILS_ROOT, 'lib', 'tasks', 'feed_importer.rake')
 
   scheduler = Rufus::Scheduler.new
 
   scheduler.interval '3m' do
-    load File.join(RAILS_ROOT, 'lib', 'tasks', 'feed_importer.rake')
     puts "next rufus run! It's #{Time.now}"
     task = Rake::Task["feed_importer:pull"] 
     task.reenable 
