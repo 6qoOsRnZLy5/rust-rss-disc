021023aeac9322bc386cfde0e3bece8e49e56237
Your Name <you@example.com>
2019-07-22 00:25:20 +0200

    lul

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,8 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.web_console.permissions = '172.19.0.0/16'
-  config.rvt.whitelisted_ips = '172.19.0.0/16'
+  config.web_console.permissions = '172.0.0.0/255.255.0.0'
+  config.rvt.whitelisted_ips = '172.0.0.0/255.255.0.0'
   config.web_console.development_only = false
 
   # Eager load code on boot. This eager loads most of Rails and
