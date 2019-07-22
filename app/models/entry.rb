75375d0a361ec10d559bb99d845fa67ffe0c5671
Your Name <you@example.com>
2019-07-22 04:10:58 +0200

    test

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -10,19 +10,19 @@ class Entry < ApplicationRecord
 
   def send_to_discord
     fragment = Nokogiri::HTML.fragment(description)
-    img = fragment.at('img').attr('src')
+    #img = fragment.at('img').attr('src')
     fragment = fragment.search('.//a').remove
     fragment = fragment.search('.//span').remove
     f = fragment.to_html
     k = Kramdown::Document.new(f, :input => 'html').to_kramdown
-    emb = { thumbnail: { url: img } }
+    #emb = { thumbnail: { url: img } }
     webhook = ENV['DISCORD_REMOTE']
     conn = Faraday.new(
          url: webhook,
          headers: {'Content-Type' => 'application/json'}
     )
     resp = conn.post do |req|
-      req.body = { username: "plebbot", content: k, embeds: emb }.to_json
+      req.body = { username: "plebbot", content: k }.to_json
     end
   end
 
