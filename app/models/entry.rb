06a2d6ee8b075c9eba016f20e6a3191a2e00009c
Your Name <you@example.com>
2019-07-22 02:57:38 +0200

    lul

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,10 +1,21 @@
 require 'kramdown'
+webhook = ENV['DISCORD_REMOTE']
 
 class Entry < ApplicationRecord
   belongs_to :feed
 
   def descriptionmd
-    Kramdown::Document.new(description).to_markdown
+    Kramdown::Document.new(description).to_kramdown
+  end
+
+  def send
+    conn = Faraday.new(
+         url: webhook,
+         headers: {'Content-Type' => 'application/json'}
+    )
+    resp = conn.post do |req|
+      req.body = { username: "plebbot", content: descriptionmd }.to_json
+    end
   end
 
 end
