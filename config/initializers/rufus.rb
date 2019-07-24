79da26fc8732a2212a51eeb1f62623fd7292d8fc
Your Name <you@example.com>
2019-07-24 04:24:03 +0200

    corasd

--- config/initializers/rufus.rb
+++ config/initializers/rufus.rb
@@ -1,37 +1,15 @@
-# require 'rufus-scheduler'
-# require 'uri'
-# require 'feedjira'
+require 'rufus-scheduler'
 
-# if ENV["OC_DEPLOYMENT"] == "YES"
 
-#   scheduler = Rufus::Scheduler.new
+if ENV["OC_DEPLOYMENT"] == "YES"
 
-#   scheduler.interval '10m' do
-#     puts "next rufus run! It's #{Time.now}"
+  scheduler = Rufus::Scheduler.new
 
-#     Feed.all.each do |feed|
-#       if feed.pullurl
-#         if feed.pullurl =~ URI::regexp
-#           puts "start syncing #{feed.title}"
-#           content = Feedjira::Feed.fetch_and_parse feed.pullurl
-#           content.entries.each do |entry|
-#             local_entry = feed.entries.where(guid: entry.guid).first_or_initialize
-#             local_entry.update_attributes(
-#               description: entry.description, 
-#               title: entry.title)
-#             puts "Synced Entry - #{entry.title}"
-#           end
-#           puts "done syncing #{feed.title}"
-#         else
-#           puts "skipping #{feed.title}, pullurl is not an url"
-#         end
-#       else
-#         puts "skipping #{feed.title}, no pullurl"
-#       end
-#     end
+   scheduler.interval '1m' do
+     puts "next rufus run! It's #{Time.now}"
+     system("curl locahost:8080/refresh && curl localhost:6060/publish")
+     puts "rufus task should be done! It's #{Time.now}"
+  end
 
-#     puts "rufus task should be done! It's #{Time.now}"
-#   end
-
-#   scheduler.join
-# end
+  scheduler.join
+end
