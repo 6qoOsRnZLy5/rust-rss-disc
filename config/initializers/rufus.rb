2a248818e488190dd5e67e14f4099b18ab6898ab
Your Name <you@example.com>
2019-07-24 04:34:47 +0200

    corasd

--- config/initializers/rufus.rb
+++ config/initializers/rufus.rb
@@ -1,15 +1,15 @@
-require 'rufus-scheduler'
+# require 'rufus-scheduler'
 
 
-if ENV["OC_DEPLOYMENT"] == "YES"
+# if ENV["OC_DEPLOYMENT"] == "YES"
 
-  scheduler = Rufus::Scheduler.new
+#   scheduler = Rufus::Scheduler.new
 
-   scheduler.interval '1m' do
-     puts "next rufus run! It's #{Time.now}"
-     system("curl locahost:8080/refresh && curl localhost:6060/publish")
-     puts "rufus task should be done! It's #{Time.now}"
-  end
+#    scheduler.interval '1m' do
+#      puts "next rufus run! It's #{Time.now}"
+#      system("curl locahost:8080/refresh && curl localhost:6060/publish")
+#      puts "rufus task should be done! It's #{Time.now}"
+#   end
 
-  scheduler.join
-end
+#   scheduler.join
+# end
