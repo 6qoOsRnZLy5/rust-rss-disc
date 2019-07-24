9138ec96ed406f42b0a8c35cf15f12afa8310d4c
Your Name <you@example.com>
2019-07-24 02:45:31 +0200

    test

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -15,14 +15,29 @@ class Entry < ApplicationRecord
     fragment.search('.//span').each {|x| x.remove}
     f = fragment.to_html
     k = Kramdown::Document.new(f, :input => 'html').to_kramdown
-    embedds = [ { title: "View this news", url: link1 }, { thumbnail: { url: img } } ]
+    k.sub!(/{.*}/, '')
+
+
+    color = 15805500
+    author = "XXXX"
+    authorf = { name: 'Author', value: author, inline: true }
+    time = "2019-07-21 01:31:02"
+    timef = { name: 'Time', value: time, inline: true }
+    ffields = [ authorf, timef ]
+    thumb = "https://steamcdn-a.akamaihd.net/steam/apps/252490/capsule_231x87.jpg"
+    thumbe = { url: thumb }
+    imagee = { url: img }
+    embededes = { title: title, url: link1, description: k, color: color, fields: ffields, thumbnail: thumbe, image: imagee }
+    embededs = [ embededes ]
+
+    #embedds = [ { title: "View this news", url: link1 }, { thumbnail: { url: img } } ]
     webhook = ENV['DISCORD_REMOTE']
     conn = Faraday.new(
          url: webhook,
          headers: {'Content-Type' => 'application/json'}
     )
     resp = conn.post do |req|
-      req.body = { username: "plebbot", content: k, embeds: embedds }.to_json
+      req.body = { username: "plebbot", embeds: embededs }.to_json
     end
   end
 
